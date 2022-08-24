7f06b946fff453c38928747dc79471e63971a191
Dedpool1998 <msmsmanishkumar64@gmail.com>
2022-08-24 12:34:02 +0530

    master

--- gusty-cover-2254/src/App.js
+++ gusty-cover-2254/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginPage from './homepage/loginPage/LoginPage';
+import logo from "./logo.svg";
+import "./App.css";
+import LoginPage from "./homepage/loginPage/LoginPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <LoginPage />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="" element={} /> */}
+        {/* <Route path="" element={} /> */}
+        {/* <Route path="" element={} /> */}
+      </Routes>
     </div>
   );
 }
