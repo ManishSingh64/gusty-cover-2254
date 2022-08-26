86e7227ec26244b2818cef4edb3755cf201ba1f6
Sagar-Gondage <sagargondage370@gmail.com>
2022-08-26 23:58:47 +0530

    day-4 added final

--- src/Components/Tasks/Static/DateConverter.jsx
+++ src/Components/Tasks/Static/DateConverter.jsx
@@ -2,9 +2,11 @@ export const ShowTime = (deadline) => {
   let milliseconds = Number(deadline.deadline);
   const dateObject = new Date(milliseconds);
   const humanDateFormat = dateObject.toLocaleString().split(",")[0];
+
+  if (deadline.deadline === undefined) {
+    return "-";
+  }
   if (humanDateFormat) {
     return humanDateFormat;
-  } else {
-    return "-";
   }
 };
