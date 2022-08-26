a80ee64ea6629de469a43743a80a03d5deae6d62
Dedpool1998 <msmsmanishkumar64@gmail.com>
2022-08-26 23:39:26 +0530

    fw17_0478 day 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import SignupPage from "./Pages/login and signup/Signup-page";
 import LoginPage from "./Pages/login and signup/LoginPage";
 import AddTask from "./Components/Tasks/AddTask";
+import { Crm } from "./Pages/CRM/Crm";
+import { Taskform } from "./Components/TaskForm/Taskform";
 
 function App() {
   return (
@@ -11,8 +13,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/tasks" element={<AddTask />} />
-        {/* <Route path="" element={} /> */}
-        {/* <Route path="" element={} /> */}
+        <Route path="/crm" element={<Crm/>} />
+        {/* <Route path="/taskform" element={<Taskform/>} /> */}
         {/* <Route path="" element={} /> */}
       </Routes>
     </div>
