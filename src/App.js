da4267fe1739228a40af84b86bc569a017fedbc7
Aaru1996 <arunkumaryadav19031996@gmail.com>
2022-08-27 05:20:22 +0530

    landing page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./Pages/HomePage/Landingpage/Homepage";
-import LoginPage from "./Pages/HomePage/loginPage/LoginPage";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <LoginPage /> */}
-      {/* <Homepage /> */}
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
+     
+      {/* <Routes> */}
+        {/* <Route path="/login" element={} /> */}
         {/* <Route path="" element={} />
         <Route path="" element={} />
         <Route path="" element={} /> */}
-      </Routes> 
+      {/* </Routes>  */}
     </div>
   );
 }
